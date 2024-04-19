d07ec2cd0de422192dfdd5dd4fb81a650274cc2b
AlonsoMarreroBello <usr3891@salesianos-lacuesta.net>
2024-04-19 10:28:06 +0100

    refactor: refactorized the methods that contains same code as others with methods from window class

--- src/net/salesianos/Menu.java
+++ src/net/salesianos/Menu.java
@@ -1,19 +1,18 @@
 package net.salesianos;
 
 import java.util.ArrayList;
-
 import javax.swing.JOptionPane;
-
 import net.salesianos.utils.Validation;
+import net.salesianos.utils.Window;
 
 public class Menu {
   public static void addRestaurant() {
-    String name = JOptionPane.showInputDialog("Enter the name of the restaurant:");
-    String location = JOptionPane.showInputDialog("Enter the location of the restaurant:");
-    String hours = JOptionPane.showInputDialog("Enter the hours of operation of the restaurant:");
+    String name = Window.askString("Enter the name of the restaurant:");
+    String location = Window.askString("Enter the location of the restaurant:");
+    String hours = Window.askString("Enter the hours of operation of the restaurant:");
     float rating;
     do {
-      String ratingStr = JOptionPane.showInputDialog("Enter the rating of the restaurant:");
+      String ratingStr = Window.askString("Enter the rating of the restaurant:");
       if (Validation.isNumber(ratingStr)) {
         rating = Float.parseFloat(ratingStr);
         break;
@@ -24,38 +23,38 @@ public class Menu {
 
     Restaurant restaurant = new Restaurant(name, location, hours, rating);
     Manager.addRestaurant(restaurant);
-    JOptionPane.showMessageDialog(null, "Restaurant added successfully.");
+    Window.showMessage("Restaurant added successfully.");
   }
 
   public static void editRestaurant() {
     ArrayList<Restaurant> restaurants = Manager.getRestaurants();
     if (Validation.isListEmpty(restaurants)) {
-      JOptionPane.showMessageDialog(null, "No restaurants to edit.");
+      Window.showMessage("No restaurants to edit.");
       return;
     }
 
-    String nameToFind = JOptionPane.showInputDialog("Enter the name of the restaurant to edit:");
+    String nameToFind = Window.askString("Enter the name of the restaurant to edit:");
     Restaurant restaurantToEdit = findRestaurantByName(nameToFind, restaurants);
 
     if (restaurantToEdit == null) {
-      JOptionPane.showMessageDialog(null, "Restaurant not found.");
+      Window.showMessage("Restaurant not found.");
       return;
     }
 
     String newName = null, newLocation = null, newHours = null;
     float newRating = -1;
 
-    newName = JOptionPane.showInputDialog("Enter the new name of the restaurant:");
-    newLocation = JOptionPane.showInputDialog("Enter the new location of the restaurant:");
-    newHours = JOptionPane.showInputDialog("Enter the new hours of operation of the restaurant:");
+    newName = Window.askString("Enter the new name of the restaurant:");
+    newLocation = Window.askString("Enter the new location of the restaurant:");
+    newHours = Window.askString("Enter the new hours of operation of the restaurant:");
     String ratingStr;
     do {
-      ratingStr = JOptionPane.showInputDialog("Enter the new rating of the restaurant:");
+      ratingStr = Window.askString("Enter the new rating of the restaurant:");
       if (Validation.isNumber(ratingStr)) {
         newRating = Float.parseFloat(ratingStr);
         break;
       } else {
-        JOptionPane.showMessageDialog(null, "Rating must be a number.");
+        Window.showMessage("Rating must be a number.");
       }
     } while (true);
 
@@ -68,13 +67,13 @@ public class Menu {
     if (newHours != null) restaurantToEdit.setHours(newHours);
     if (newRating != -1) restaurantToEdit.setRating(newRating);
 
-    JOptionPane.showMessageDialog(null, "Restaurant edited successfully.");
+    Window.showMessage("Restaurant edited successfully.");
   }
 
   public static void deleteRestaurant() {
     ArrayList<Restaurant> restaurants = Manager.getRestaurants();
     if (Validation.isListEmpty(restaurants)) {
-      JOptionPane.showMessageDialog(null, "No restaurants to delete.");
+      Window.showMessage("No restaurants to delete.");
       return;
     }
 
@@ -82,7 +81,7 @@ public class Menu {
     Restaurant restaurantToDelete = findRestaurantByName(nameToDelete, restaurants);
 
     if (restaurantToDelete == null) {
-      JOptionPane.showMessageDialog(null, "Restaurant not found.");
+      Window.showMessage("Restaurant not found.");
       return;
     }
 
@@ -90,13 +89,13 @@ public class Menu {
     if (choice == JOptionPane.YES_OPTION) {
       int indexToDelete = restaurants.indexOf(restaurantToDelete);
       Manager.deleteRestaurant(indexToDelete);
-      JOptionPane.showMessageDialog(null, "Restaurant deleted successfully.");
+      Window.showMessage("Restaurant deleted successfully.");
     }
   }
 
   public static Restaurant findRestaurantByName(String name, ArrayList<Restaurant> restaurants) {
     if (!Validation.isValidString(name)) {
-      JOptionPane.showMessageDialog(null, "Invalid restaurant name.");
+      Window.showMessage("Invalid restaurant name.");
       return null;
     }
 
--- src/net/salesianos/utils/Window.java
+++ src/net/salesianos/utils/Window.java
@@ -10,11 +10,6 @@ public class Window {
   public static String askString(String message) {
     return JOptionPane.showInputDialog(message);
   }
-  
-  public static float askFloat(String message) {
-    float number = Float.parseFloat(JOptionPane.showInputDialog(message));
-    return number;
-  }
 
   public static void showMessage(String message) {
     JOptionPane.showMessageDialog(null, message);
