2b76bf413165e28404d594aa19b2e0af3fc0e430
AlonsoMarreroBello <usr3891@salesianos-lacuesta.net>
2024-04-24 20:36:21 +0100

    refactor(App): Code formatted to resolve indentation errors.

--- src/App.java
+++ src/App.java
@@ -10,36 +10,35 @@ public class App {
       int choice = 0;
       do {
         String choiceStr = Window.askString(
-          "Menu:\n" +
-              "1. Add restaurant\n" +
-              "2. Edit restaurant\n" +
-              "3. Show restaurants\n" +
-              "4. Delete restaurant\n" +
-              "5. Exit"
-      );
-      if (Validation.isValidString(choiceStr)) {
-        choice = Integer.parseInt(choiceStr);
-        
-        switch (choice) {
-          case 1:
-            Menu.addRestaurant();
-            break;
-          case 2:
-            Menu.editRestaurant();
-            break;
-          case 3:
-            Manager.showRestaurants();
-            break;
-          case 4:
-            Menu.deleteRestaurant();
-            break;
-          case 5:
-            System.out.println("Exiting the program...");
-            break;
-          default:
-            System.out.println("Invalid option.");
+            "Menu:\n" +
+                "1. Add restaurant\n" +
+                "2. Edit restaurant\n" +
+                "3. Show restaurants\n" +
+                "4. Delete restaurant\n" +
+                "5. Exit");
+        if (Validation.isValidString(choiceStr)) {
+          choice = Integer.parseInt(choiceStr);
+
+          switch (choice) {
+            case 1:
+              Menu.addRestaurant();
+              break;
+            case 2:
+              Menu.editRestaurant();
+              break;
+            case 3:
+              Manager.showRestaurants();
+              break;
+            case 4:
+              Menu.deleteRestaurant();
+              break;
+            case 5:
+              System.out.println("Exiting the program...");
+              break;
+            default:
+              System.out.println("Invalid option.");
+          }
         }
-      }
       } while (choice != 5);
     } catch (Exception e) {
       System.out.println("An unexpected error occurred: " + e.getMessage());
